928c87b2cf7dd5c1efbbdfcfae924b5e7c6d164c
keyready <vip.korchak@list.ru>
2024-02-24 04:01:00 +0300

    пошел спать

--- client/src/pages/CreateDonationPage/ui/CreateDonationPage/CreateDonationPage.tsx
+++ client/src/pages/CreateDonationPage/ui/CreateDonationPage/CreateDonationPage.tsx
@@ -1,7 +1,10 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Page } from 'widgets/Page';
-import { memo } from 'react';
+import { memo, useEffect, useState } from 'react';
 import { BreadCrumbs } from 'shared/UI/BreadCrumbs';
+import { Text } from 'shared/UI/Text';
+import { HStack, VStack } from 'shared/UI/Stack';
+import { Dropdown, DropdownChangeEvent } from 'primereact/dropdown';
 import classes from './CreateDonationPage.module.scss';
 
 interface CreateDonationPageProps {
@@ -13,10 +16,29 @@ const CreateDonationPage = memo((props: CreateDonationPageProps) => {
 
     const items: string[] = ['Добавить донацию'];
 
+    const [stationsNames, setStationsNames] = useState<string[]>([]);
+    const [selectedStation, setSelectedStation] = useState<string>('');
+
+    useEffect(() => {
+        fetch(
+            'https://api2.donorsearch.org/api/blood_stations/map/?bbox1=59.744410786472635,29.592746835620193&bbox2=60.2096799591528,30.948185068042072',
+        )
+            .then((res) => res.json())
+            .then((res) => setStationsNames(res.map((result: any) => result.title)));
+    }, []);
+
     return (
         <Page className={classNames(classes.CreateDonationPage, {}, [className])}>
             <BreadCrumbs items={items} />
-            <h1>Создать донацию</h1>
+            <h1>Добавить донацию</h1>
+            <HStack maxW className={classes.stations}>
+                <Dropdown
+                    value={selectedStation}
+                    onChange={(e: DropdownChangeEvent) => setSelectedStation(e.value)}
+                    options={stationsNames}
+                    filter
+                />
+            </HStack>
         </Page>
     );
 });
