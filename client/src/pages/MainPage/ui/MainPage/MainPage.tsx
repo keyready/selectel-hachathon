56c9c676e4bcd36dd9ce6017813a2326bd6e8dba
keyready <vip.korchak@list.ru>
2024-02-24 12:16:53 +0300

    еще раз пробую тг аус

--- client/src/pages/MainPage/ui/MainPage/MainPage.tsx
+++ client/src/pages/MainPage/ui/MainPage/MainPage.tsx
@@ -22,6 +22,7 @@ const MainPage = () => {
     useEffect(() => {
         if (location?.hash) {
             const authResult = location.hash.split('=');
+            console.log(authResult);
             toast.success(location.hash);
             if (authResult?.length) {
                 // const authData = JSON.parse(atob(location.hash.split('=')[1]));
@@ -36,7 +37,7 @@ const MainPage = () => {
     }, [location.hash, navigate]);
 
     const handleTelegramLoginClick = useCallback(() => {
-        // const yourBotId: string = '6453776863';
+        // const yourBotId: string = '7107142378';
         // const authUrl = `https://oauth.telegram.org/auth/url?bot_id=${yourBotId}&origin=${RoutePath.menu}`;
         // window.location.href = authUrl;
     }, []);
